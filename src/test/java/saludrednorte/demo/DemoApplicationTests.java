3d292ea16063ff18c6d92f6359276e7913b0cc60
Matiasv117 <matiasbvb13@gmail.com>
2026-03-18 17:08:54 -0300

    commit inicial

---
+++ src/test/java/saludrednorte/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package saludrednorte.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
